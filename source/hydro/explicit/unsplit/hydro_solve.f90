a9e1d8e2983a81e675e74d6173b0bfa6af9cb631
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2017-04-20 15:05:37 +0200

    bugfix for unsplit solver

--- source/hydro/explicit/unsplit/hydro_solve.f90
+++ source/hydro/explicit/unsplit/hydro_solve.f90
@@ -15,10 +15,10 @@ subroutine hydro_solve(dt)
  integer :: i,j,k
  real,dimension(nvar,nx+1) :: F_l,F_r
  !
- if(ndim>1) then
+ if(ndim>2) then
     write(*,*) '------------------------'
     write(*,*) 'ERROR in Hydro_solve:'
-    write(*,*) 'only 1D possilbe!'
+    write(*,*) 'only 1D and 2D possilbe!'
     write(*,*) '------------------------'
     stop
  endif
@@ -29,12 +29,14 @@ subroutine hydro_solve(dt)
    do j=jbg,jeg
       call fill_guardcells_1D(dens(:,j,k),pres(:,j,k),eint(:,j,k),u(:,j,k),v(:,j,k),w(:,j,k),ibg,ieg,ieg,bc)
    enddo  
- enddo  
- !do j=jbg,jeg
- !  do i=ibg,ieg
- !     call fill_guardcells_1D(dens(i,j,:),pres(i,j,:),eint(i,j,:),u(i,j,:),v(i,j,:),w(i,j,:),kbg,keg,keg,2)
- !  enddo  
- !enddo  
+ enddo 
+ if(ndim>1) then 
+   do k=kbg,keg
+     do i=ibg,ieg
+        call fill_guardcells_1D(dens(i,:,k),pres(i,:,k),eint(i,:,k),u(i,:,k),v(i,:,k),w(i,:,k),jbg,jeg,jeg,bc)
+     enddo  
+   enddo
+ endif
  call hydro3D(dt)
  !do k=kb,ke
  !  do j=jb,je
